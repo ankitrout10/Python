8aab1c7f70dab54f13b812a83fcb877607eb8745
1000118438 <mailme.ankitrout@gmail.com>
2023-03-08 22:05:26 +0530

    I added upto Class Object and Refrence Variable

---
+++ mathmodule.py
@@ -0,0 +1,22 @@
+pi=3.14
+a=100
+b=200
+c=300
+
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+
+print(add(10,20))
+    
+  
